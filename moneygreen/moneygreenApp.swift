3b43f6665365a48f17ce2a60e5c89955b9d50b72
Harshit Jain <harshitjain23897@gmail.com>
2024-04-27 18:06:40 +0530

    Initial Commit

---
+++ moneygreen/moneygreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  moneygreenApp.swift
+//  moneygreen
+//
+//  Created by Harshit Jain on 27/04/24.
+//
+
+import SwiftUI
+
+@main
+struct moneygreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
